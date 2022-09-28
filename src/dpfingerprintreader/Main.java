a759e64d044f2d9ead2577d23634f7a04b5e7282
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-09-28 14:39:53 -0400

    Verification class worked

--- src/dpfingerprintreader/Main.java
+++ src/dpfingerprintreader/Main.java
@@ -9,6 +9,9 @@ package dpfingerprintreader;
  * @author TTruc
 */
 
+import com.digitalpersona.uareu.*;
+import java.util.Base64;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,11 +21,55 @@ public class Main {
      * @param args the command line arguments
     */
     
-    public static void main(String[] args) throws JSONException {
+    public static void main(String[] args) {
         // TODO code application logic here
         
-        Main main = new Main();
-        System.out.print(main.enrollment());
+        //Main main = new Main();
+        //System.out.print(main.enrollment());
+        
+        // Verification verification = new Verification();
+        // verification.decodeFMDs();
+    }
+    
+    public String verification(String encodedFMDs) throws JSONException, UareUException {
+        Verification verification = new Verification(encodedFMDs);
+        verification.start();
+        
+        Verification.VerificationEvent data = verification.getVerificationEvent();
+        JSONObject json = new JSONObject();
+        
+        try {
+            json.put("verificationStatus", data.verificationStatus);
+            json.put("errorMessage", data.errorMessage);
+        } catch(JSONException e) {
+            json.put("errorMessage", "Ocurrió un error al obtener los datos del enrolamiento");
+        }
+  
+        return json.toString();
+        
+        /*
+        JSONObject json = new JSONObject(encodedFMDs);
+        
+        JSONObject currentFMD = new JSONObject(json.getJSONObject("currentFMD").toString());
+        
+        JSONArray  jsonArr = new JSONArray(json.getJSONArray("FMDs").toString());
+        JSONObject jsonObj = new JSONObject();
+        
+        JSONArray jsonArrTest = new JSONArray();
+        
+        for (int i = 0; i < jsonArr.length(); i++) {
+            jsonObj = jsonArr.getJSONObject(i);
+            
+            byte[] decode = Base64.getDecoder().decode(jsonObj.getString("data"));
+            String format = jsonObj.getString("format"); 
+            
+            Fmd fmd = UareUGlobal.GetImporter().ImportFmd(decode, Fmd.Format.valueOf(format), Fmd.Format.ANSI_378_2004);
+            
+            jsonArrTest.put(Base64.getEncoder().encodeToString(fmd.getData()));
+        }
+        
+        return currentFMD.toString();
+        */
     }
     
     public String enrollment() throws JSONException {
---
+++ src/dpfingerprintreader/Verification.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dpfingerprintreader;
+
+/**
+ *
+ * @author TTruc
+*/
+
+import com.digitalpersona.uareu.*;
+import java.util.Base64;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Verification {
+    
+    private VerificationEvent verificationEvent = null;
+    private Fmd               fmd               = null;
+    private Fmd[]             fmds              = null;
+    private String            encodedFMDs       = null;
+    
+    Verification(String encoded) {
+        encodedFMDs = encoded;
+    }
+    
+    public class VerificationEvent {
+        boolean verificationStatus = false;
+        String  errorMessage       = null;
+        
+        VerificationEvent(boolean status, String errorMsg) {
+            verificationStatus = status;
+            errorMessage       = errorMsg;
+        }
+    }
+    
+    public void decodeFMDs() throws JSONException, UareUException {
+        JSONObject json = new JSONObject(encodedFMDs);
+        
+        JSONArray  jsonArr = new JSONArray(json.getJSONArray("FMDs").toString());
+        JSONObject jsonObj = new JSONObject();
+        
+        fmds = new Fmd[jsonArr.length()];
+        
+        // JSONArray jsonArrTest = new JSONArray();
+        
+        for (int i = 0; i < jsonArr.length(); i++) {
+            jsonObj = jsonArr.getJSONObject(i);
+            
+            byte[] decode = Base64.getDecoder().decode(jsonObj.getString("data"));
+            String format = jsonObj.getString("format"); 
+            
+            Fmd someFMD = UareUGlobal.GetImporter().ImportFmd(decode, Fmd.Format.valueOf(format), Fmd.Format.ANSI_378_2004);
+            fmds[i] = someFMD;
+            
+            // jsonArrTest.put(Base64.getEncoder().encodeToString(fmd.getData()));
+        }
+        
+        JSONObject currentFMDObject = new JSONObject(json.getJSONObject("currentFMD").toString());
+        
+        byte[] decode = Base64.getDecoder().decode(currentFMDObject.getString("data"));
+        String format = jsonObj.getString("format"); 
+        
+        Fmd currentFMD = UareUGlobal.GetImporter().ImportFmd(decode, Fmd.Format.valueOf(format), Fmd.Format.ANSI_378_2004);  
+        fmd = currentFMD;
+    }
+    
+    private void eventListener(boolean status, String errorMsg) {
+        VerificationEvent event = new VerificationEvent(status, errorMsg);
+        verificationEvent = event;
+    }
+    
+    public void verification() throws JSONException, UareUException {
+        Engine engine = UareUGlobal.GetEngine();
+        boolean isMatched = false;
+        
+        for (int i = 0; i < fmds.length; i++) {
+            try {
+                int falsematch_rate = engine.Compare(fmd, 0, fmds[i], 0);
+                int target_falsematch_rate = Engine.PROBABILITY_ONE / 100000;
+
+                if (falsematch_rate < target_falsematch_rate){
+                    isMatched = true;
+                    break;
+                } else {
+                    isMatched = false;
+                }
+            } catch(UareUException e) {
+                System.out.print(e);
+            }
+        }
+        
+        if (isMatched) {
+            eventListener(isMatched, "La huella se encuentra registrada");
+        } else {
+            eventListener(isMatched, "");
+        }
+    }
+    
+    public VerificationEvent getVerificationEvent() {
+        return verificationEvent;
+    }
+    
+    public void start() throws JSONException, UareUException {
+        decodeFMDs();
+        verification();
+    }
+    
+}
